99c7963f88b3828b30e79b1c384547fd3bbe4251
shahanneda <shahan.neda@gmail.com>
2021-02-21 11:10:23 -0800

    added domain for demo purposes

--- client/src/js/main.jsx
+++ client/src/js/main.jsx
@@ -5,7 +5,7 @@ const wrapper = document.getElementById("react-cont");
 
 ReactDOM.render(
         <div>
-            { (location.hostname === "localhost" || location.hostname === "127.0.0.1") ? 
+            { (location.hostname === "hunterhub.net" || location.hostname === "127.0.0.1") ? 
             <HackerHub url="http://127.0.0.1:4564" />
             :<HackerHub url="http://192.252.235.8:4564"/>
           }
